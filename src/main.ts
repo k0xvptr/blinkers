7001f8e48c1871ffe9ea52210afb9202ce7f8c2e
Kingsley Leon <kingsleyleoncs@gmail.com>
2026-06-15 16:37:18 -0400

    feat: New HTML code uploaded, widget bug of taking up half-screen fixed

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
+import { getCurrentWindow, LogicalSize } from '@tauri-apps/api/window';
 
-const pauseButton = document.getElementById('btn-pause');
+const appWindow = getCurrentWindow();
+const toggleDrawerBtn = document.getElementById('btn-toggle-drawer');
+const drawer = document.getElementById('creation-drawer');
+const drawerIcon = document.getElementById('icon-drawer');
+const createForm = document.getElementById('form-create-container');
 
-function handlePauseClick() {
+let isDrawerOpen = false;
 
+function handleAdd() {
+		
 }
 
 interface TaskContainer {
